5534f0f32f200d73caedeb3424a9571df3b2dc95
zang206 <jprocknow@gmail.com>
2017-12-09 11:30:16 -0600

    updates

--- HB_DashCam_Converter_Beta-Tester.sh
+++ HB_DashCam_Converter_Beta-Tester.sh
@@ -68,10 +68,13 @@ fi
 #---------------------------------------------------------------------
 #  files and paths needed to run script from command line
 #
-#   this folder has 2 subdirs and 3 avi files
-#   cd /media/jason/Backup2/VidConvert/DashCams/Black32GBSD/10-21-2017_Black/DCIM
+#   Location of files to be converted
+#   -Folder used for testing beta versions of the script
+#   --this folder has 2 subdirs and 3 avi files
+#   ---cd /media/jason/Backup2/VidConvert/DashCams/Black32GBSD/10-21-2017_Black/DCIM
 #   cd /media/jason/Backup2/VidConvert/DashCams
-#
+#   
+#   Location of script
 #   /home/jason/git_repositories/HandBrake_Scripts/HandbrakeProcessTreeGoodOneMaybe.sh
 
 
